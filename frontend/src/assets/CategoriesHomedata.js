f0795e683ca6f9fc64b88f666cece619f2c52272
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-16 12:41:09 +0500

    first commit

---
+++ frontend/src/assets/CategoriesHomedata.js
@@ -0,0 +1,25 @@
+// src/data/brandsData.js
+import Rolex from "../assets/Rolex.png"
+import Omega from "../assets/Omega.png"
+import AudemarsPiguet from "../assets/AudemarsPiguet.png"
+import PatekPhilippe from "../assets/PatekPhilippe.png"
+import Cartier from "../assets/Cartier.png"
+import Brietling from "../assets/Brietling.webp"
+import IWC from "../assets/IWC.png"
+import Hublot from "../assets/HuBLOT.PNG"
+import TagHeuer from "../assets/TagHeuer.png"
+import JaegerLeCoultre from "../assets/JaegerLeCoultre.png"
+const brands = [
+  { id: 1, name: "Rolex", slug: "rolex", image: Rolex, link: "/brands/rolex" },
+  { id: 2, name: "Omega", slug: "omega", image: Omega, link: "/brands/omega" },
+  { id: 3, name: "Patek Philippe", slug: "patek-philippe", image: PatekPhilippe, link: "/brands/patek-philippe" },
+  { id: 4, name: "Audemars Piguet", slug: "audemars-piguet", image: AudemarsPiguet, link: "/brands/audemars-piguet" },
+  { id: 5, name: "Cartier", slug: "cartier", image: Cartier, link: "/brands/cartier" },
+  { id: 6, name: "Breitling", slug: "breitling", image: Brietling, link: "/brands/breitling" },
+  { id: 7, name: "IWC", slug: "iwc", image: IWC, link: "/brands/iwc" },
+  { id: 8, name: "Hublot", slug: "hublot", image: Hublot, link: "/brands/hublot" },
+  { id: 9, name: "Tag Heuer", slug: "tag-heuer", image: TagHeuer, link: "/brands/tag-heuer" },
+  { id: 10, name: "Jaeger-LeCoultre", slug: "jaeger-lecoultre", image:JaegerLeCoultre, link: "/brands/jaeger-lecoultre" },
+];
+
+export default brands;
---
+++ frontend/src/assets/dummywdata.js
@@ -0,0 +1,117 @@
+import W1 from "../assets/W1.png"
+import W2 from "../assets/W2.png"
+import W3 from "../assets/W3.png"
+import W4 from "../assets/W4.png"
+import W5 from "../assets/W5.png"
+import W6 from "../assets/W6.png"
+import W7 from "../assets/W7.png"
+import W8 from "../assets/W8.png"
+import W9 from "../assets/W9.png"
+import W10 from "../assets/W10.png"
+import W11 from "../assets/W11.png"
+import W12 from "../assets/W12.png"
+
+export const WATCHES = [
+  {
+    id: 1,
+    name: "JACOB & CO. EPIC X",
+    desc: "Automatic • Sport Strap",
+    price: "₹ 6,19,000",
+    img: W1,
+    gender: "men",
+  },
+  {
+    id: 2,
+    name: "H. MOSER & CIE.",
+    desc: "Chronograph • Leather",
+    price: "₹ 10,69,200",
+    img: W2,
+    gender: "men",
+  },
+  {
+    id: 3,
+    name: "BVLGARI OCTO",
+    desc: "Limited Edition • Bold",
+    price: "₹ 31,00,000",
+    img: W3,
+    gender: "men",
+  },
+  {
+    id: 4,
+    name: "IWC PORTUGIESER",
+    desc: "Skeleton • Titanium",
+    price: "₹ 24,50,000",
+    img:W4,
+    gender: "men",
+  },
+  {
+    id: 5,
+    name: "BELL & ROSS URBAN",
+    desc: "Minimal • Purple Dial",
+    price: "₹ 33,00,000",
+    img:W5,
+    gender: "men",
+  },
+  {
+    id: 6,
+    name: "D1 MILANO",
+    desc: "Classic • Dress",
+    price: "₹ 38,40,000",
+    img: W6,
+    gender: "men",
+  },
+  {
+    id: 7,
+    name: "LONGINES ELEGANCE",
+    desc: "Quartz • Mesh Strap",
+    price: "₹ 2,99,000",
+    img:W7,
+    gender: "women",
+  },
+  {
+    id: 8,
+    name: "RADO CENTRIX",
+    desc: "Slim • Steel",
+    price: "₹ 1,49,000",
+    img: W8,
+    gender: "women",
+  },
+  {
+    id: 9,
+    name: "BVLGARI SERPENTI",
+    desc: "Slim • Steel",
+    price: "₹ 1,49,000",
+    img: W9,
+    gender: "women",
+  },
+  {
+    id: 10,
+    name: "ORIS AQUIS",
+    desc: "Slim • Steel",
+    price: "₹ 1,49,000",
+    img: W10,
+    gender: "women",
+  },
+  {
+    id: 11,
+    name: "OMEGA",
+    desc: "Slim • Steel",
+    price: "₹ 1,49,000",
+    img:W11,
+    gender: "women",
+  },
+  {
+    id: 12,
+    name: "MASERATI LIFESTYLE",
+    desc: "Slim • Steel",
+    price: "₹ 1,49,000",
+    img: W12,
+    gender: "women",
+  },
+];
+
+export const FILTERS = [
+  { key: "all", label: "All", iconName: "Grid" },
+  { key: "men", label: "Men", iconName: "User" },
+  { key: "women", label: "Women", iconName: "Users" },
+];
