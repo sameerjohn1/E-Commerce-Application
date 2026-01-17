903b91271405525c931a010d92d89d01cb6f9ea3
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-17 12:07:16 +0500

    feat: add BannerHome component with cards, text content

--- frontend/src/components/BannerHome.jsx
+++ frontend/src/components/BannerHome.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useRef } from "react";
 import { bannerHomeStyles } from "../assets/dummyStyles";
 import Navbar from "./Navbar";
 import video from "../assets/bannervideo.mp4";
+import BL1 from "../assets/BL1.png";
+import BM1 from "../assets/BM1.png";
+import BR1 from "../assets/BR1.png";
 
 const BannerHome = () => {
   const videoRef = useRef(null);
@@ -38,6 +41,92 @@ const BannerHome = () => {
         >
           <source src={video} type="video/mp4" />
         </video>
+      </div>
+
+      {/* content */}
+      <div className={bannerHomeStyles.contentContainer}>
+        <div className={bannerHomeStyles.h1Container}>
+          <h1
+            style={bannerHomeStyles.playfairFont}
+            className={bannerHomeStyles.h1Text}
+          >
+            <span className={bannerHomeStyles.h1SpanGray}>Love you more</span>
+
+            <span className={bannerHomeStyles.h1SpanYellow}>
+              with each tick-tok
+            </span>
+          </h1>
+
+          <p className={bannerHomeStyles.subtext}>
+            Discover our exclusive collection of handcrafted timeplaces that
+            embodyprecision, luxury, and timeless style.
+          </p>
+        </div>
+
+        {/* card section */}
+        <div className={bannerHomeStyles.cardsContainer}>
+          <div className={bannerHomeStyles.grid}>
+            <div
+              className={`${bannerHomeStyles.cardWrapper} ${bannerHomeStyles.leftCardTransform}`}
+            >
+              <div
+                className={`${bannerHomeStyles.cardBase} ${bannerHomeStyles.cardPadding}`}
+              >
+                <img
+                  src={BL1}
+                  alt="left logo"
+                  className={`${bannerHomeStyles.cardImage} ${bannerHomeStyles.leftCardImage}`}
+                  loading="lazy"
+                />
+              </div>
+              <p
+                className={`${bannerHomeStyles.cardLabel} ${bannerHomeStyles.cardLabelGray}`}
+              >
+                Classic Heritage
+              </p>
+            </div>
+
+            <div
+              className={`${bannerHomeStyles.cardWrapper} ${bannerHomeStyles.middleCardTransform}`}
+            >
+              <div
+                className={`${bannerHomeStyles.cardMiddle} ${bannerHomeStyles.cardPadding}`}
+              >
+                <img
+                  src={BM1}
+                  alt="middle logo"
+                  className={`${bannerHomeStyles.cardImage} ${bannerHomeStyles.middleCardImage}`}
+                  loading="lazy"
+                />
+              </div>
+              <p
+                className={`${bannerHomeStyles.cardLabel} ${bannerHomeStyles.cardLabelYellow}`}
+              >
+                Limited Edition
+              </p>
+            </div>
+
+            <div
+              className={`${bannerHomeStyles.cardWrapper} ${bannerHomeStyles.rightCardTransform}`}
+            >
+              <div
+                className={`${bannerHomeStyles.cardBase} ${bannerHomeStyles.cardPadding}`}
+              >
+                <img
+                  src={BR1}
+                  alt="left logo"
+                  className={`${bannerHomeStyles.cardImage} ${bannerHomeStyles.rightCardImage}`}
+                  loading="lazy"
+                />
+              </div>
+              <p
+                className={`${bannerHomeStyles.cardLabel} ${bannerHomeStyles.cardLabelGray}`}
+              >
+                Modern Precision
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
