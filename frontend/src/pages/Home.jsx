f8391d00826ed2f2e1471ed9a3928adf5ba18e7a
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-19 12:11:34 +0500

    feat: add CategoriesHome component with brand grid and hover effect

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BannerHome from "../components/BannerHome";
+import CategoriesHome from "../components/CategoriesHome";
 
 const Home = () => {
   return (
     <div>
       <BannerHome />
+      <CategoriesHome />
     </div>
   );
 };
