d7b0722a51fc7ffe095cb64d7b900a15450e3b43
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-20 12:21:04 +0500

    feat: add login page with validation, toast & local auth simulation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Brand from "./pages/Brand";
+import LoginPage from "./components/LoginPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/brands/:brandName" element={<Brand />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
