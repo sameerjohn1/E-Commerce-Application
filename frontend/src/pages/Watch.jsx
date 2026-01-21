08a58a2afb9782cfa182e84ee732b59c402a3c6e
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-21 18:44:41 +0500

    feat: add watch listing page with filter and cart controls

---
+++ frontend/src/pages/Watch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import WatchPage from "../components/WatchPage";
+
+const Watch = () => {
+  return (
+    <div>
+      <Navbar />
+      <WatchPage />
+      <Footer />
+    </div>
+  );
+};
+
+export default Watch;
