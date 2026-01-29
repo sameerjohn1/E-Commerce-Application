b824e078f027b3a6dfaaab7e5369b0b5c0451654
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-29 16:34:09 +0500

    feat(admin): add manage booking page with filters and status update

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/auth", userRouter);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/api/watches", watchRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Api working");
